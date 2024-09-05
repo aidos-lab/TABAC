df0272f665ae8964e1d54c8259224ceb7628e564
julius-vr <44684046+julius-vr@users.noreply.github.com>
2024-09-05 09:07:18 +0200

    Update run_experiments_vicsek.py
    Configure experiments

--- tabac/run_experiments_vicsek.py
+++ tabac/run_experiments_vicsek.py
@@ -7,10 +7,12 @@ res_list_importance_topological = []
 res_list_MCMC_scc = []
 res_list_MCMC_topological = []
 
+num_simulations = "750"
+
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", "250", "--sampler", "importance", "--distance", "hausdorff"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", num_simulations, "--sampler", "importance", "--distance", "hausdorff"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_importance_scc.append(inner_list)
@@ -18,7 +20,7 @@ for theta in tqdm(["0.15","0.3","0.6"]):
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", "250", "--sampler", "importance", "--distance", "topological"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", num_simulations, "--sampler", "importance", "--distance", "topological"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_importance_topological.append(inner_list)
@@ -26,7 +28,7 @@ for theta in tqdm(["0.15","0.3","0.6"]):
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", "250", "--sampler", "MCMC", "--distance", "hausdorff"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", num_simulations, "--sampler", "MCMC", "--distance", "hausdorff"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_MCMC_scc.append(inner_list)
@@ -34,12 +36,12 @@ for theta in tqdm(["0.15","0.3","0.6"]):
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", "250", "--sampler", "MCMC", "--distance", "topological"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "vicsek", "--theta", theta, "-n", "2000", "-N", num_simulations, "--sampler", "MCMC", "--distance", "topological"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_MCMC_topological.append(inner_list)
 
 res_dic = {"Vicsek Importance hausdorff":res_list_importance_scc,"Vicsek Importance topological":res_list_importance_topological,"Vicsek MCMC hausdorff":res_list_MCMC_scc,"Vicsek MCMC topological":res_list_MCMC_topological}
 
-with open('vicsek_50.pkl', 'wb') as fp:
+with open('vicsek_50_'+num_simulations+'.pkl', 'wb') as fp:
     pickle.dump(res_dic, fp)
