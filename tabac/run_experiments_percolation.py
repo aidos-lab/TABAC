8080f5633c4776f73c6c80c00abc6b594dec282d
julius-vr <44684046+julius-vr@users.noreply.github.com>
2024-09-05 09:08:49 +0200

    Update run_experiments_percolation.py
    Configure experiments

--- tabac/run_experiments_percolation.py
+++ tabac/run_experiments_percolation.py
@@ -7,10 +7,12 @@ res_list_importance_topological = []
 res_list_MCMC_scc = []
 res_list_MCMC_topological = []
 
+num_simulations = "750"
+
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", "250", "--sampler", "importance", "--distance", "scc"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", num_simulations, "--sampler", "importance", "--distance", "scc"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_importance_scc.append(inner_list)
@@ -18,7 +20,7 @@ for theta in tqdm(["0.15","0.3","0.6"]):
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", "250", "--sampler", "importance", "--distance", "cubical"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", num_simulations, "--sampler", "importance", "--distance", "cubical"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_importance_topological.append(inner_list)
@@ -26,7 +28,7 @@ for theta in tqdm(["0.15","0.3","0.6"]):
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", "250", "--sampler", "MCMC", "--distance", "scc"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", num_simulations, "--sampler", "MCMC", "--distance", "scc"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_MCMC_scc.append(inner_list)
@@ -34,12 +36,12 @@ for theta in tqdm(["0.15","0.3","0.6"]):
 for theta in tqdm(["0.15","0.3","0.6"]):
     inner_list = []
     for i in range(5):
-        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", "250", "--sampler", "MCMC", "--distance", "cubical"], capture_output=True, text=True)
+        result = subprocess.run(["python", "experiments_auxiliary.py", "--shape", "perc", "--theta", theta, "50", "-n", "100", "-N", num_simulations, "--sampler", "MCMC", "--distance", "cubical"], capture_output=True, text=True)
         fin_res = float(result.stdout)
         inner_list.append(fin_res)
     res_list_MCMC_topological.append(inner_list)
 
 res_dic = {"Perc Importance SCC":res_list_importance_scc,"Perc Importance topological":res_list_importance_topological,"Perc MCMC SCC":res_list_MCMC_scc,"Perc MCMC topological":res_list_MCMC_topological}
 
-with open('perc.pkl', 'wb') as fp:
+with open('perc_'+num_simulations+'.pkl', 'wb') as fp:
     pickle.dump(res_dic, fp)
